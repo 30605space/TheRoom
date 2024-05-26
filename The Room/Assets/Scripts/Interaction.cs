100f51d397761ff29f28c254528e93689ecc365a
Twook_1120 <ptw061120@naver.com>
2024-05-26 20:14:03 +0900

    123
    ddfd

--- The Room/Assets/Scripts/Interaction.cs
+++ The Room/Assets/Scripts/Interaction.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Interaction : MonoBehaviour
 {
-    public Text interactionText;
+    public Text interactionText;   
 
     void Update()
     {
@@ -16,13 +16,20 @@ public class Interaction : MonoBehaviour
     {
         if(other.gameObject.layer == 6)
         {
-            interactionText.text = "(E) " + other.gameObject.name;
+            interactionText.text = "(E) " + other.gameObject.name; 
+            if(Input.GetKeyDown(KeyCode.E) && other.gameObject.GetComponent<Animator>().GetBool("DoorOpen") == false)
+            {              
+                other.gameObject.GetComponent<Animator>().SetBool("DoorOpen", true);
+            }
+            if (Input.GetKeyDown(KeyCode.E) && other.gameObject.GetComponent<Animator>().GetBool("DoorOpen") == true)
+            {
+                other.gameObject.GetComponent<Animator>().SetBool("DoorOpen", false);
+            }
         }              
     }
     void OnTriggerExit(Collider other)
     {
         interactionText.text = "";
     }
-
 
 }
